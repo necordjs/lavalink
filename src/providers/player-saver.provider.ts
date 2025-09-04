d6278e333f2a3029275a905fee13d98cc7884a7f
NedcloarBR <nedcloar1@hotmail.com>
2025-09-03 23:00:00 -0300

    fix: use provider type

--- src/providers/player-saver.provider.ts
+++ src/providers/player-saver.provider.ts
@@ -1,8 +1,9 @@
+import { Provider } from '@nestjs/common';
 import { BaseStore } from '../helpers';
 import { PlayerStore } from '../constants';
 import { PlayerSaver } from '../services';
 
-export const PlayerSaverProvider = {
+export const PlayerSaverProvider: Provider<PlayerSaver> = {
 	provide: PlayerSaver,
 	useFactory: (store: BaseStore) => new PlayerSaver(store),
 	inject: [PlayerStore]
--- src/providers/player-store.provider.ts
+++ src/providers/player-store.provider.ts
@@ -1,8 +1,10 @@
+import { Provider } from '@nestjs/common';
 import { PlayerStore } from '../constants';
 import { NecordLavalinkModuleOptions } from '../necord-lavalink-options.interface';
 import { LAVALINK_MODULE_OPTIONS } from '../necord-lavalink.module-definition';
+import { BaseStore } from '../helpers';
 
-export const PlayerStoreProvider = {
+export const PlayerStoreProvider: Provider<BaseStore> = {
 	provide: PlayerStore,
 	useFactory: (options: NecordLavalinkModuleOptions) => options.autoResume?.playerStore,
 	inject: [LAVALINK_MODULE_OPTIONS]
