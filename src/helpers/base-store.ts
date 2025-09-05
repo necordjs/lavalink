46bfd4757641c351c2a7af60cbdc395e656de7bf
Miguel Alexandre Uhlein <nedcloar1@hotmail.com>
2025-09-04 22:07:28 -0300

    fix: import service (#369)

--- src/helpers/base-store.ts
+++ src/helpers/base-store.ts
@@ -1,9 +1,11 @@
+import { PlayerJson } from 'lavalink-client';
+
 export abstract class BaseStore {
-	public abstract save(key: string, value: any): Promise<void>;
+	public abstract save(key: string, value: string): Promise<void>;
 
 	public abstract delete(key: string): Promise<void>;
 
-	public abstract get(key: string): Promise<any>;
+	public abstract get(key: string): Promise<PlayerJson | null>;
 
 	public abstract getAll(): Promise<{ key: string; value: string }[]>;
 }
